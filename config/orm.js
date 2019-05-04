441f45c5a7e9f1f4887632907da2575184cf76ce
Amir P <apourrezaei@gmail.com>
2019-05-04 19:44:41 -0400

    working on post route

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,16 @@ var orm = {
             cb(result);
             
         });
+    },
+    insertOne:function(name, devoured) {
+        var table = "burgers";
+        var col1 = "burger_name";
+        var col2 = "devoured";
+
+        var queryStr = "INSERT INTO " = table + "(";
+        queryStr += col1 + "," + col2 + ")";
+        queryStr += " Values(" + name + ", " + devored + ");";
+        console.log(queryStr);  
     }
    
 
--- models/burger.js
+++ models/burger.js
@@ -5,6 +5,11 @@ var burger = {
         orm.selectAll("burgers", function(res) {
             cb(res);
         })
+    },
+    insertOne: function(n, d, cb ) {
+        orm.insertOne(n,d, function(res) {
+            cb(res);
+        })
     }
 
 }
